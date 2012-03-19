490b8d638d065c009182f54aa58e65b6474188af
pixelles <patriciadupelle@msn.com>
2012-03-19 19:13:32 -0400

    Added some CSS.

--- js/tabs.js
+++ js/tabs.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	
 	$('.tabs a').on('click', function(ev){
 		var currentTab = $(this).attr('href');
-			
-	
+				
 		$('.tab-content .current').removeClass('current');		
 		
 		$('.tab-content div:not(.current)').hide(300);
 		$(currentTab).addClass('current').show(300);	
 
 	});
+
 });
 
